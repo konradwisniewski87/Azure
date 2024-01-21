44a3b6ac663e78bafbe051641e8a671b3a5fb773
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2024-01-21 17:02:53 +0100

    Adding upload and download file from/to Azure

---
+++ BlobStorage/Controllers/BlobStorageController.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlobStorage.Controllers
+{
+	[ApiController]
+	[Microsoft.AspNetCore.Components.Route("[controller]")]
+	public class BlobStorageController : ControllerBase
+	{
+		[HttpPost("upload")]
+		public async Task<IActionResult> Upload(IFormFile file)
+		{
+			var connectionString = "DefaultEndpointsProtocol=https;AccountName=storage1sane;AccountKey=jzmbliQaH8pHjCoiKb5PFKCJld2+vSc8HyqL34dGsXy2XOj8Nmrx3bbQcS2uGyrPfhP5YEgt9dCr+AStPiTX4w==;EndpointSuffix=core.windows.net";
+			BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+			var containerName = "documents";
+			BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+			await containerClient.CreateIfNotExistsAsync();
+
+			BlobClient blobClient = containerClient.GetBlobClient(file.FileName);
+
+			var blobHttpHeaders = new BlobHttpHeaders();
+			blobHttpHeaders.ContentType = file.ContentType;
+
+			//await blobClient.UploadAsync(file.OpenReadStream()); //return error when file exist
+			//await blobClient.UploadAsync(file.OpenReadStream(), overwrite: true);
+			await blobClient.UploadAsync(file.OpenReadStream(), blobHttpHeaders);//overwrite as default
+
+			return Ok();
+		}
+
+		[HttpGet("download")]
+		public async Task<IActionResult> Download([FromQuery]string blobName)
+		{
+			var connectionString = "DefaultEndpointsProtocol=https;AccountName=storage1sane;AccountKey=jz...w==;EndpointSuffix=core.windows.net";
+			BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+			var containerName = "documents";
+			BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+			await containerClient.CreateIfNotExistsAsync();
+
+			BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+			var downloadResponse = await blobClient.DownloadContentAsync();
+			var content = downloadResponse.Value.Content.ToStream();
+			var contentType = blobClient.GetProperties().Value.ContentType;
+
+			return File(content, contentType, fileDownloadName: blobName);
+		}
+    }
+
+	
+}
